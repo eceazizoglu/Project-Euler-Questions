9e20213a86034e32314b3f88351d67af3a6b9b2d
Ece Azizoglu <30914557+eceazizoglu@users.noreply.github.com>
2017-10-20 23:00:27 +0300

    Add files via upload

---
+++ sum-of-mult-3-5.rkt
@@ -0,0 +1,15 @@
+(define (add-helper lst)
+  (if (null? lst) 0
+      (+ (car lst) (add-helper (cdr lst)))))
+
+(define (list-of-mult-3-5 n)
+  (if (= n 0) '()
+      (if (or (= (remainder (- n 1) 3) 0) (= (remainder (- n 1) 5) 0))
+          (cons (- n 1) (list-of-mult-3-5 (- n 1)))
+          (list-of-mult-3-5 (- n 1)))))
+
+(define (sum-of-mult-3-5 n)
+ (add-helper (list-of-mult-3-5 n)))
+            
+
+
